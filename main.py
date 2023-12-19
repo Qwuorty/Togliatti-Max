fdb44ff93311fbf218054060233718f43967de14
Qwuorty <103215069+Qwuorty@users.noreply.github.com>
2023-12-19 22:51:43 +0400

    task 3

--- main.py
+++ main.py
@@ -1,59 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5 import uic
-import random
+from random import randint
+
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtCore import Qt
+from draw_c import Ui_Dialog
+from PyQt5.QtWidgets import QDialog, QApplication
 
+class Yellow(QDialog, Ui_Dialog):
+    def init(self):
+        super().init()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.toggle_drawing)
+        self.drawing_enabled = False  # Флаг для отслеживания, нужно ли рисовать
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        # Загрузка интерфейса из файла UI.ui
-        uic.loadUi('UI.ui', self)
-
-        # Создание кнопки
-        self.button = QPushButton('Нажми меня', self)
-        self.button.clicked.connect(self.on_button_click)
-
-        # Создание виджета для отображения окружностей
-        self.circle_widget = CircleWidget()
-
-        # Расположение компонентов на форме
-        layout = QVBoxLayout()
-        layout.addWidget(self.button)
-        layout.addWidget(self.circle_widget)
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
-
-    def on_button_click(self):
-        # При нажатии кнопки добавляем случайную окружность на виджет
-        diameter = random.randint(10, 100)
-        self.circle_widget.add_circle(diameter)
-
-
-class CircleWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.circles = []
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        for circle in self.circles:
-            painter.setBrush(QColor(255, 255, 0))  # желтый цвет
-            painter.drawEllipse(circle[0], circle[1], circle[2], circle[2])
-
-    def add_circle(self, diameter):
-        x = random.randint(0, self.width() - diameter)
-        y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+    def toggle_drawing(self):
+        self.drawing_enabled = True
         self.update()
 
+    def paintEvent(self, event):
+        self.qp = QPainter(self)
+        if self.drawing_enabled:
+            self.draw()
+        self.qp.end()
 
-if __name__ == '__main__':
+    def draw(self):
+        R = randint(20, 100)
+        x = randint(0, self.width() - R)
+        y = randint(0, self.height() - R)
+        self.qp.setBrush(QColor(*[randint(0, 255) for _ in range(3)]))
+        self.qp.drawEllipse(x, y, R, R)
+
+
+if name == 'main':
     app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
+    ex = Yellow()
+    ex.show()
     sys.exit(app.exec_())
